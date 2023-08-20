6cf170d9b9d458d731dd506418a6c0d15ed0f169
KakaoSoup <kspaul47@gmail.com>
2023-08-20 12:48:51 +0900

    [new] : signal generator with spare structure

--- RC_MUX.v
+++ RC_MUX.v
@@ -8,21 +8,20 @@ module RC_MUX (
 );
 
 parameter mask = 3'b111;
+reg [2:0] idx[0:3];
 
 always @ (posedge clk) begin
-    assign repair_addr[0] <= PCAM_addr[(find_idx(dsss) >> 9) & mask];
-    assign repair_addr[1] <= PCAM_addr[(find_idx(dsss) >> 6) & mask];
-    assign repair_addr[2] <= PCAM_addr[(find_idx(dsss) >> 3) & mask];
-    assign repair_addr[3] <= PCAM_addr[find_idx(dsss) & mask];
+    repair_addr[0] <= PCAM_addr[(find_idx(dsss) >> 9) & mask];
+    repair_addr[1] <= PCAM_addr[(find_idx(dsss) >> 6) & mask];
+    repair_addr[2] <= PCAM_addr[(find_idx(dsss) >> 3) & mask];
+    repair_addr[3] <= PCAM_addr[find_idx(dsss) & mask];
 end
 
 function [11:0] find_idx;
 input [7:0] dsss;
     begin
-        integer [2:0] i;
-        integer [2:0] idx[0:3];
+        integer i;
         integer cnt = 0;
-
         for(i = 0; i < 8; i = i + 1) begin
             if(dsss[7-i]) begin
                 idx[cnt] = i;
@@ -32,4 +31,5 @@ input [7:0] dsss;
     end
     find_idx = { idx[0], idx[1], idx[2], idx[3] };
 endfunction
+
 endmodule
